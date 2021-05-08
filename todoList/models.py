fe4e9ea674f9cc238ff41b2c7b446003e7701002
Deryu99 <dank9928@gmail.com>
2021-05-08 02:28:09 +0200

    Navbar and css/style work now! still working on the DB

--- todoList/models.py
+++ todoList/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# @Author Daniel C (all commented for now)
+
+# We want a @oneToMany rel. for User and todoItem -> use foreing key
+
+# class PercentageField(fields.FloatField):
+#     widget = fields.TextInput(attrs={"class": "percentInput"})
+
+#     def to_python(self, value):
+#         val = super(PercentageField, self).to_python(value)
+#         if is_number(val):
+#             return val/100
+#         return val
+
+#     def prepare_value(self, value):
+#         val = super(PercentageField, self).prepare_value(value)
+#         if is_number(val) and not isinstance(val, str):
+#             return str((float(val)*100))
+#         return val
+
+
+# # model for todos
+# class todoItem(model.Model):
+#     description = models.TextField(max_length=160)
+#     deadline = models.DateTimeField()
+#     percent = models.IntegerField()
+
+#     # @param User: Related Table
+#     # @pam on_delete: if User gets delete, then delete todoItem (-->)
+#     author = model.ForeingKey(User, on_delete=models.CASCADE)
--- todoList/views.py
+++ todoList/views.py
@@ -1,9 +1,36 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# dummy todos
+todos = [
+    {
+        'author': 'Daniel',
+        'description': 'My first todo item!',
+        'percentage': '44',
+        'deadline': 'February 28, 2022'
+    },
+    {
+        'author': 'Burak',
+        'description': 'My Second todo item!',
+        'percentage': '77',
+        'deadline': 'July 15, 2022'
+    },
+    {
+        'author': 'Max',
+        'description': 'My third todo item!',
+        'percentage': '99',
+        'deadline': 'December 31, 2022'
+    }
+]
+
+
+
 # My views. still not linked between them
 def index(request):
-    return render(request, 'todoList/index.html')
+    context = {
+        'todos': todos
+    }
+    return render(request, 'todoList/index.html', context)
 
 
 def how_to_edit_todo(request):
